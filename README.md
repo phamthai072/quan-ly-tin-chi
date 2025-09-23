ed4b4bdeca383cc91349d2f24e3b0765301abbed
phamthainb <phamthai4643@gmail.com>
2025-09-24 01:25:27 +0700

    refactor: Update navigation menu titles for clarity and consistency

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Firebase Studio
+# Note
 
-This is a NextJS starter in Firebase Studio.
-
-To get started, take a look at src/app/page.tsx.
+- Thêm lớp học phần mới -> check gv theo môn học -> chỉ những gv thuộc khoa của môn học mới được chọn
--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -199,39 +199,3 @@ CREATE TABLE
         ten NVARCHAR (100) PRIMARY KEY,
         gia_tri NVARCHAR (200) NOT NULL
     );
-
--- Thêm từng dòng một
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'DiemQuaMon', N'4');
-
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'GPA_CanhBao', N'2');
-
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'TinChiTruot_CanhBao', N'5');
-
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'GV_ToiDa_Lop_HK', N'8');
-
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'GV_ToiThieu_Tiet', N'48');
-
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'MonNgoaiNganh_ToiDa', N'3');
-
-INSERT INTO
-    cau_hinh (ten, gia_tri)
-VALUES
-    (N'TinChi_ToiDa_HocKy', N'20');
